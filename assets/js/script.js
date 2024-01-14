86609b54f99aa9306cd4a0ea1e5198ecd4927c5a
Adrian Tut <adriantutuianu01@gmail.com>
2024-01-14 23:35:58 +0000

    Created weather card + temperature

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,37 @@ const cityInput = $("#search-input");
 const cityForm = $("#search-form");
 const APIkey = "d80a5e97b418450696733535d1602cdf";
 
-// const cityName = "Sheffield";
 const BASE_URL_BY_CITY = `https://api.openweathermap.org/geo/1.0/direct`;
-// Using the 5 Day Weather Forecast API, you'll notice that you will need to pass in coordinates instead of just a city name.
-// Using the OpenWeatherMap APIs, how could we retrieve geographical coordinates given a city name?
+
+// Weather card
+function createWeatherCard(data) {
+  const card = $("<div>").addClass("card");
+  //Card header - city name
+  //used if statement to avoid errors in console
+  if (data.city && data.city.name) {
+    const cardHeader = $("<div>").addClass("card-header").text(data.city.name);
+    card.append(cardHeader);
+  }
+  //card body
+  const cardBody = $("<div>").addClass("card-body");
+  //card body temperature
+  if (data.list && data.list.length > 0 && data.list[0].main) {
+    const temperatureKelvin = data.list[0].main.temp;
+    const temperatureCelsius = (temperatureKelvin - 273.15).toFixed(2);
+
+    const temperatureParagraph = $("<p>")
+      .addClass("card-text")
+      .text(`Temperature: ${temperatureCelsius}°C`);
+
+    //append to card body
+    cardBody.append(temperatureParagraph);
+  }
+
+  card.append(cardBody);
+
+  return card;
+}
+
 function getForecastData(lat, lon) {
   const FORECAST_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIkey}`;
 
@@ -16,6 +43,9 @@ function getForecastData(lat, lon) {
     })
     .then(function (data) {
       console.log(data);
+
+      // Update the weatherContainer with the forecast data
+      updateWeather(data);
     });
 }
 
@@ -39,7 +69,51 @@ function getCityData(event) {
       //get lan and lon
       console.log("Latitude: ", lat.toFixed(2));
       console.log("Longitude: ", lon.toFixed(2));
+
+      const todayContainer = $("#today");
+      todayContainer.empty();
+
+      const card = createWeatherCard(data);
+      todayContainer.append(card);
     });
 }
 
 cityForm.on("submit", getCityData);
+
+function updateWeather(data) {
+  const todayContainer = $("#today");
+  todayContainer.empty();
+
+  const card = createWeatherCard(data);
+  todayContainer.append(card);
+}
+
+// function updateWeather(data) {
+//   const todayContainer = $("#today");
+
+//   // Clear the previous content
+//   todayContainer.empty();
+
+//   //Create a Bootstrap card
+//   const card = $("<div>").addClass("card");
+
+//   //Display the city name in card header
+//   const cardHeader = $("<div>").addClass("card-header").text(data.city.name);
+//   card.append(cardHeader);
+
+//   // Display temperature in card body
+//   //temp in kelvin
+//   const temperatureKelvin = data.list[0].main.temp;
+//   //temp in celsius
+//   const temperatureCelsius = (temperatureKelvin - 273.15).toFixed(2);
+//   console.log(temperatureCelsius);
+
+//   const cardBody = $("<div>").addClass("card-body");
+//   const temperatureParagraph = $("<p>")
+//     .addClass("card-text")
+//     .text(`Temperature: ${temperatureCelsius}°C`);
+//   cardBody.append(temperatureParagraph);
+
+//   // Append the card to the todayContainer
+//   todayContainer.append(card);
+// }
