9f22499bc63ffb8244bd8dfd0f3e5a9ef64f6c1c
Adrian Tut <adriantutuianu01@gmail.com>
2024-01-14 21:53:29 +0000

    Get forecast data function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,18 @@ const APIkey = "d80a5e97b418450696733535d1602cdf";
 const BASE_URL_BY_CITY = `https://api.openweathermap.org/geo/1.0/direct`;
 // Using the 5 Day Weather Forecast API, you'll notice that you will need to pass in coordinates instead of just a city name.
 // Using the OpenWeatherMap APIs, how could we retrieve geographical coordinates given a city name?
+function getForecastData(lat, lon) {
+  const FORECAST_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIkey}`;
+
+  fetch(FORECAST_URL)
+    .then(function (resp) {
+      return resp.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
+
 function getCityData(event) {
   event.preventDefault();
   const cityName = cityInput.val();
@@ -21,6 +33,8 @@ function getCityData(event) {
       console.log(data);
       const lat = data[0].lat;
       const lon = data[0].lon;
+
+      getForecastData(lat, lon);
 
       //get lan and lon
       console.log("Latitude: ", lat.toFixed(2));
